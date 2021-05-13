d39aa7bafec18fc06fcce116e0a6d75463c8bbd8
Derek Louis <thedereklouis@gmail.com>
2021-05-13 08:42:24 -0400

    updated example blog posts

--- src/myBlogs.js
+++ src/myBlogs.js
@@ -1,10 +1,10 @@
 const myBlogs = [
   {
-    path: 'pizza',
-    title: 'Loving Pizza',
+    path: 'blog1',
+    title: 'Example Blog 1',
     date: '3/12/21',
     teaser:
-      'This is the very descriptive teaser for this very well written blog post!',
+      'This is a teaser for example blog number 1. Real blog posts are on the way!',
     body: [
       'It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.',
       'Don’t worry about sounding professional. Sound like you. There are over 1.5 billion websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.',
@@ -12,11 +12,11 @@ const myBlogs = [
     ],
   },
   {
-    path: 'cookies',
-    title: 'Making Cookies',
+    path: 'blog2',
+    title: 'Example Blog 2',
     date: '5/15/21',
     teaser:
-      'This is the very descriptive teaser for this very well written blog post!',
+      'This is a teaser for example blog number 2. Real blog posts are on the way!',
     body: [
       'It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.',
       'Don’t worry about sounding professional. Sound like you. There are over 1.5 billion websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.',
@@ -24,11 +24,11 @@ const myBlogs = [
     ],
   },
   {
-    path: 'icecream',
-    title: 'Tasting Icecream',
+    path: 'blog3',
+    title: 'Example Blog 3',
     date: '6/4/21',
     teaser:
-      'This is the very descriptive teaser for this very well written blog post!',
+      'This is a teaser for example blog number 3. Real blog posts are on the way!',
     body: [
       'It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.',
       'Don’t worry about sounding professional. Sound like you. There are over 1.5 billion websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.',
@@ -36,11 +36,11 @@ const myBlogs = [
     ],
   },
   {
-    path: 'cheeseburgers',
-    title: 'Eating CheeseBurgers',
+    path: 'blog4',
+    title: 'Example Blog 4',
     date: '11/24/21',
     teaser:
-      'This is the very descriptive teaser for this very well written blog post!',
+      'This is a teaser for example blog number 4. Real blog posts are on the way!',
     body: [
       'It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.',
       'Don’t worry about sounding professional. Sound like you. There are over 1.5 billion websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.',
